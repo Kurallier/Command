19e64f4f348b4f35476c599adc5b340deffff0ee
Cal Eagle <cal97eagle@gmail.com>
2026-03-06 21:32:34 +0000

    modData info

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = ;
+name = "Command Pack";
+timestamp = 5249472623762853344;
---
+++ mod.cpp
@@ -0,0 +1,16 @@
+// Mod Details
+
+name		 	= "Command Pack";									// Name of your mod
+author			= "https://steamcommunity.com/id/CEagle";
+
+picture 	 	= ""; 											// Picture displayed from the expansions menu. Optimal size is 2048x1024
+logoSmall	 	= "Command_Logo_128_ca.paa";							// Display next to the item added by the mod
+logo		 	= "Command_Logo_128_ca.paa";								// Logo displayed in the main menu
+logoOver	 	= "Command_Logo_128_ca.paa";							// When the mouse is over, in the main menu
+
+tooltipOwned 	= "Command Pack";									// Tool tip displayed when the mouse is left over, in the main menu
+action			= "";				// Website URL, that can accessed from the expansions menu
+dlcColor[]		= {0.31,0.77999997,0.77999997,1};				// Color used for DLC stripes and backgrounds (RGBA)
+dir				= "Command Pack";
+
+
